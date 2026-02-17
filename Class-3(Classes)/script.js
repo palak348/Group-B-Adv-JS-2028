32405fb83327cb4e87a7603f1d3120627b065df6
mrinal1224 <iammrinalkc@gmail.com>
2026-02-17 11:45:52 +0530

    push

--- Class-3(Classes)/script.js
+++ Class-3(Classes)/script.js
@@ -1,10 +1,22 @@
 class Pizaa {
+   static totalPizzaMade = 0;
+   #size
+   
+
+
   constructor(size, toppings, preference, crust) {
-    this.size = size;
+    this.#size = size;
     this.toppings = toppings;
     this.preference = preference;
     this.crust = crust;
+    Pizaa.totalPizzaMade++
   }
+
+  static showTotalPizza(){
+    console.log(`${Pizaa.totalPizzaMade}`)
+  }
+
+  // Closures - Next Class
 
   serve() {
     console.log(`This is a ${this.size} Pizza from parent `);
@@ -27,19 +39,29 @@ class StuffedCrustPizaa extends Pizaa {
 }
 
 const order1 = new Pizaa("Medium", ["Tomato , Cheese"], "Veg", "Thin");
-console.log(order1);
+console.log(order1.size);
+const order3 = new Pizaa("Medium", ["Tomato , Cheese"], "Veg", "Thin");
+console.log(order3);
+const order4 = new Pizaa("Medium", ["Tomato , Cheese"], "Veg", "Thin");
+console.log(order4);
+const order5 = new Pizaa("Medium", ["Tomato , Cheese"], "Veg", "Thin");
+console.log(order5);
 
-const order2 = new StuffedCrustPizaa(
-  "small",
-  ["mushrooms", "cheese"],
-  "Veg",
-  "Thick",
-  "Mozarella"
-);
+// // const order2 = new StuffedCrustPizaa(
+// //   "small",
+// //   ["mushrooms", "cheese"],
+// //   "Veg",
+// //   "Thick",
+// //   "Mozarella"
+// // );
 
-console.log(order2);
+// console.log(order2);
 
-order1.serve();
-order2.describe();
+// order1.serve();
+// order2.describe();
+
+Pizaa.showTotalPizza()
 
 // Classcial Inhertance in JS (prototypal)
+// Static properties and methods
+
